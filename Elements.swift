8ca5788c46883a270506d2a0b6fc2c80114f8a69
HelloWorld174 <83816889+HelloWorld174@users.noreply.github.com>
2021-05-12 16:43:50 +0500

    Update Elements.swift

--- Elements.swift
+++ Elements.swift
@@ -1,3 +1,50 @@
-HStack{
-content
+HStack(alignment: .top,
+      spacing: 10
+      ) {
+ ForEach(1...5,
+         id: \self
+        ){
+      Text("Item \($0)")
+    }
+  }
 }
+
+VStack(alignment: .top,
+      spacing: 10
+      ) {
+ ForEach(1...5,
+         id: \self
+        ){
+      Text("Item \($0)")
+    }
+  }
+}
+
+ZStack{
+  VStack(alignment: .top,
+         spacing: 10
+        ) {
+  ForEach(1...5,
+         id: \self
+        ){
+        Text("Item \($0)")
+      }
+    }
+  }
+}
+
+ScrollView(.horizontal) {
+  LazyHStack(alignment: .top, spacing: 10) {
+    ForEach(1...100, id: \.self) {
+      Text("Column" \($0)")
+            }
+         }
+      }
+  
+  ScrollView {
+  LazyVStack(alignment: .leading) {
+    ForEach(1...100, id: \.self) {
+      Text("row" \($0)")
+            }
+         }
+      }
